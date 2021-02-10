7bc2720e1c030b90c5ecbe8e770acb04731cff66
George Serna <sernageorge@gmail.com>
2021-02-10 03:35:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == (" ") || ("") || (nil)
     return false
-  elsif board[index] == "X" || "O"
+  elsif index == "X" || "O"
     return true
   end
 end

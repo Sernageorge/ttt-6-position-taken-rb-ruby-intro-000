a864d4ca922c4ae3c6d50a09904e743579ff9dcd
George Serna <sernageorge@gmail.com>
2021-02-10 03:09:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " "
-  elsif board[index] == nil
-  elsif board[index] == ""
+  if board[index] != " "
+  elsif board[index] != nil
+  elsif board[index] != ""
   end
 end
